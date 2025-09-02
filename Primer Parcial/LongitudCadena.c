181acf6d458fbbe93cf111ae82ef8cbe6668a34f
QVCI <carloivan2910@gmail.com>
2025-09-02 00:49:17 -0600

    feat Contar Caracteres
    Se agrego un conteo de caracteres

--- Primer Parcial/LongitudCadena.c
+++ Primer Parcial/LongitudCadena.c
@@ -4,13 +4,26 @@ int main ()
 {
     char Cadena [40], * Ptr;
     int Long = 0;
+    int SinEspacios = 0;
     printf("Ingrese un texto: ");
 
     gets(Cadena);
 
-    for(Ptr = Cadena; *Ptr != '\0'; Ptr = Ptr + 1)
+    /* 
+        Ptr = Cadena -> Indice 0
+        Mientras el char de Ptr sea diferente del caracter de fin
+        Pasar al siguiente espacio del arreglo y aumentar en 1 la Longitud de la cadena
+
+    */
+    for(Ptr = Cadena; *Ptr != '\0'; Ptr = Ptr + 1, Long++)
     {
-        Long ++;
+        // Buscar espacios para contarlos
+        if (*Ptr == ' ')
+        {
+            SinEspacios++;
+        }
+        
     }
-    printf("La longitud de la cadena es de: %d", Long);
+    // Restar los espacios a la longitud de la cadena
+    printf("La longitud total de la cadena es de: %d, y tiene: %d caracteres.", Long, Long - SinEspacios);
 }
