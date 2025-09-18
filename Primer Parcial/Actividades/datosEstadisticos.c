4354a744471405547ab20763d85deb98a48d4b11
QVCI <carloivan2910@gmail.com>
2025-09-18 09:01:02 -0600

    ya con funciones

--- Primer Parcial/Actividades/datosEstadisticos.c
+++ Primer Parcial/Actividades/datosEstadisticos.c
@@ -7,6 +7,8 @@ struct datoEst
     float promedio, desvEstandar;
     int num;    //datos Ingresados
 };
+int IngresarDato(struct datoEst*Ptr);
+int ImprimeDato(struct datoEst*Ptr);
 int main()
 {
     struct datoEst misdatos, *Ptrs;
@@ -25,56 +27,15 @@ int main()
         scanf("%d", &opc);
         if (opc == 1)
         {
-            if (Ptrs->num < TAM) 
-            {
-                int valor;
-                printf("\nIngrese un numero: ");
-                scanf("%d", &valor);
-
-                Ptrs->Arreglo[Ptrs->num] = valor;
-               
-                Ptrs->num++;
-                 printf("\nValor numero %d de %d", (Ptrs->num), TAM);
-            }
-            else
-            {
-                printf("\nEl arreglo ya esta lleno, no se pueden agregar ms valores.\n");
-            }
+            IngresarDato(Ptrs);
+           
         }
 
 
         if (opc == 2)
         {
-            float suma = 0;
-            printf("\nDatos en lista: ");
-            for (Ptrs->Ptr = Ptrs->Arreglo; 
-                Ptrs->Ptr < Ptrs->Arreglo + Ptrs->num; 
-                Ptrs->Ptr++)
-            {
-                suma += *Ptrs->Ptr;
-               
-
-                if (Ptrs->Ptr == Ptrs->Arreglo)
-                    printf("%d", *Ptrs->Ptr);
-
-                else
-                    printf(", %d", *Ptrs->Ptr);
-            }
+            ImprimeDato(Ptrs);
             
-            Ptrs->promedio = suma / Ptrs->num;
-            printf("\nEl Promedio es: %.2f", Ptrs->promedio);
-
-          
-            float sumaCuadrados = 0;
-            for (Ptrs->Ptr = Ptrs->Arreglo; 
-                Ptrs->Ptr < Ptrs->Arreglo + Ptrs->num; 
-                Ptrs->Ptr++) 
-            {
-                sumaCuadrados += pow(*Ptrs->Ptr - Ptrs->promedio, 2);
-            }
-            Ptrs->desvEstandar = sqrt(sumaCuadrados / Ptrs->num);
-            printf("\nLa Desviacion Estandar es: %.2f", Ptrs->desvEstandar);
-
         }
 
       
@@ -83,5 +44,62 @@ int main()
 
     
     return 0;
+}
+int IngresarDato(struct datoEst*Ptr)
+{
+    if (Ptr->num < TAM) 
+    {
+        int valor;
+        printf("\nIngrese un numero: ");
+        scanf("%d", &valor);
+
+        Ptr->Arreglo[Ptr->num] = valor;
+        
+        Ptr->num++;
+        printf("\nValor numero %d de %d", (Ptr->num), TAM);
+    }     
+    else
+    {
+        printf("\nEl arreglo ya esta lleno, no se pueden agregar ms valores.\n");
+    }
+
+    return 0;
+}
+int ImprimeDato(struct datoEst*Ptrs)
+{
+    float suma = 0;
+    printf("\nDatos en lista: ");
+    for (Ptrs->Ptr = Ptrs->Arreglo; 
+    Ptrs->Ptr < Ptrs->Arreglo + Ptrs->num; 
+    Ptrs->Ptr++)
+    {
+        suma += *Ptrs->Ptr;  
+
+        if (Ptrs->Ptr == Ptrs->Arreglo)
+        {          
+            printf("%d", *Ptrs->Ptr);
+        }        
+        else
+        {         
+            printf(", %d", *Ptrs->Ptr);
+        }
+            
+    }
+                  
+    Ptrs->promedio = suma / Ptrs->num;
+    printf("\nEl Promedio es: %.2f", Ptrs->promedio);
+
+    float sumaCuadrados = 0;
+            
+    for (Ptrs->Ptr = Ptrs->Arreglo;         
+        Ptrs->Ptr < Ptrs->Arreglo + Ptrs->num;         
+        Ptrs->Ptr++)    
+        {     
+            sumaCuadrados += pow(*Ptrs->Ptr - Ptrs->promedio, 2);     
+        }   
+    Ptrs->desvEstandar = sqrt(sumaCuadrados / Ptrs->num);
+           
+    printf("\nLa Desviacion Estandar es: %.2f", Ptrs->desvEstandar);
+
 }
 
